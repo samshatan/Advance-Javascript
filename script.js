b2c5182c372d46937e6a952a7a23283325cd15a7
samshatan <sameermaurya97044@gmail.com>
2025-04-09 16:00:57 +0530

    Third Class

--- script.js
+++ script.js
@@ -107,3 +107,125 @@
 //     resultarr.push(result);
 // }
 // console.log(resultarr)
+
+// Variables scope in function
+
+// function testAvailiability(x){
+//     console.log("Avialable here :",x);
+// }
+// testAvailiability("Hi");
+// console.log("Not Avaiable here: ",x);
+
+// function testAvailiability(){
+//     let y = "Local Variable"
+//     console.log("Avialable here :",y);
+// }
+// testAvailiability();
+// console.log("Not Avaiable here: ",y);
+
+// function testAvailiability(){
+//     let y = "I'll return";
+//     console.log("AVailable here:",y);
+//     return y;
+// }
+// let z = testAvailiability();
+// console.log("Outside the function: ",z);
+// console.log("Not Avaiable here: ",y);
+
+// let versus var variable
+
+// function doingStuff(){
+//     if(true){
+//         var x = "local";
+//     }
+//     console.log(x);
+// }
+// doingStuff();
+
+// function doingStuff(){
+//     if(true){
+//         console.log(x);
+//         var x = "local";
+//     }
+// }
+// doingStuff();
+
+// constant scope 
+
+// function doingStuff(){
+//     if(true){
+//         const x ="Local";
+//         console.log(x);
+//     }
+//     console.log(x);
+// }
+// doingStuff();
+
+// global variable
+
+// let globalvariable = "Accessible Everywhere";
+// console.log("Outside Function ",globalvariable);
+// function creatingNewScope(x){
+//     console.log("Acces to global vars inside function ",globalvariable);
+// }
+// creatingNewScope("Some parameter");
+// console.log("Still Available",globalvariable);
+
+// let x = "global"
+// function doingStuff(){
+//     let x = "local";
+//     console.log(x);
+// }
+// doingStuff()
+// console.log(x);
+
+// let x = "global";
+// function doingStuff(x){
+//     console.log(x);
+// }
+// doingStuff("param");
+// console.log(x);
+
+// function confuseReader(){
+//     x = "Guess my scope";
+//     console.log("Inside the function ",x);
+// }
+// confuseReader();
+// console.log("Outside the fuction ",x);
+
+// Immegiately Invoked Fuction Expression (IIFE is a way of expressing a function so that it gets invoked immediately. It is anonymous, it doesnt have a name, and it is self-executing
+
+// (function () {
+//     console.log("IIFE");
+// })
+// ();
+
+// (()=>{
+//     console.log("run right away");
+// })();
+
+// (function (x,y){
+//     console.log(x+y);
+// })(4,5);
+
+// Recursive Function 
+
+// function getRecursive(nr){
+//     console.log(nr);
+//     if(nr>0){
+//         getRecursive(--nr);
+//     }
+// }
+// getRecursive(3);
+
+function logRecursive(nr){
+    console.log("Started Function ",nr);
+    if(nr>0){
+        logRecursive(nr-1);
+    }
+    else{
+        console.log("DOne with recursion");
+    }
+    console.log("Ended function",nr);
+}
+logRecursive(3);
